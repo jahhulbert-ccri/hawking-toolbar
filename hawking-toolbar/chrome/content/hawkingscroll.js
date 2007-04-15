25c84854eb8a9427e86ce2e93f1754464109dd35
Hasuob <Hasuob@94e5ff19-e727-0410-9be4-bf860293b04e>
2007-04-15 06:09:04 +0000

    Autoscrolling testing

--- hawking-toolbar/chrome/content/hawkingscroll.js
+++ hawking-toolbar/chrome/content/hawkingscroll.js
@@ -12,15 +12,17 @@ function htbToggleAuto(){
 
 function htbEnableAuto(){
 	htbSetPref("autoMode", true, "Bool");
-	var timing = parseInt(htbGetPref("autoInterval"));
+	var timing = parseFloat(htbGetPref("autoInterval"));
 	if(timing<1 || isNaN(timing)){
 		timing = 2;
 	}
 	timing = timing*1000;//from seconds to miliseconds
 	autoInterval = setInterval("ContextManager.next();", timing);
+//	alert(autoInterval+" set");
 }
 
 function htbDisableAuto(){
 	htbSetPref("autoMode", false, "Bool");
+//	alert("got: "+autoInterval);
 	clearInterval(autoInterval);
 }
