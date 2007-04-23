81c5df0377394b4026007134d20564ec601b0a91
hasuob <hasuob@94e5ff19-e727-0410-9be4-bf860293b04e>
2007-04-23 20:54:11 +0000

    added a defaults directory which SHOULD set up default preferences for the startup user. though i'm not sure if it actually works. will test further

---
+++ hawking-toolbar/chrome/defaults/preferences/defaultprefs.js
@@ -0,0 +1,5 @@
+pref('extensions.hawking.soundClick','clickon.wav');
+pref('extensions.hawking.soundNext','next.wav');
+pref('extensions.hawking.soundPrev','prev.wav');
+pref('extensions.hawking.soundoff', false);
+//http://developer.mozilla.org/en/docs/Building_an_Extension#Defaults_Files
